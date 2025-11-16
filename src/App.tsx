919393fd450d7734c308cce9823668a07b500d1c
mohammed sobhi <Mosobhi01@gmail.com>
2025-11-16 03:46:24 +0200

    `Update favicon and adjust grid layout`

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className='flex flex-col h-screen'>
     <Header/>
-    <div className="grid grid-cols-1 lg:grid-cols-3 overflow-hidden flex-1 bg-[#060606]">
-      <div className="hidden lg:block border-t border-b lg:border-r border-green-500">
+    <div className="grid grid-cols-1 lg:grid-cols-7 overflow-hidden flex-1 bg-[#060606]">
+      <div className="hidden lg:col-span-3 lg:block border-t border-b lg:border-r border-green-500">
         <ThreeCard/>
       </div>
-      <div className="col-span-1 lg:col-span-2 border-t  border-b lg:border lg:border-r-transparent border-green-500 h-full bg-black">
+      <div className="col-span-1 lg:col-span-4 border-t  border-b lg:border lg:border-r-transparent border-green-500 h-full bg-black">
         <Terminal />
       </div>
     </div>
